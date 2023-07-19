373c54b80a466e072584a2c944feed716981a5ce
“eliaspoylio” <elias.poylio@gmail.com>
2023-07-19 11:59:06 +0300

    Add text & refactor project structure

---
+++ src/effects/text.rs
@@ -0,0 +1,72 @@
+//#[path = "../vector.rs"] mod vector;
+use crate::{
+    matrix,
+    vector::{create_line_float, draw_points_float, Point, VertexF},
+};
+
+use std::collections::HashMap;
+
+const SIZE_F: f32 = crate::SIZE_F;
+
+pub fn letter(coord: (f32, f32), size: f32) -> Vec<(Point)> {
+    let font: [(char, Vec<(Point, Point)>); 2] = [
+        (
+            'c',
+            vec![
+                (Point::new(2., 0.), Point::new(7., 0.)),
+                (Point::new(1., -1.), Point::new(3., -1.)),
+                (Point::new(6., -1.), Point::new(8., -1.)),
+                (Point::new(0., -2.), Point::new(2., -2.)),
+                (Point::new(7., -2.), Point::new(9., -2.)),
+                (Point::new(0., -3.), Point::new(2., -3.)),
+                (Point::new(7., -3.), Point::new(9., -3.)),
+                (Point::new(0., -4.), Point::new(2., -4.)),
+                (Point::new(0., -5.), Point::new(2., -5.)),
+                (Point::new(0., -6.), Point::new(2., -6.)),
+                (Point::new(7., -6.), Point::new(9., -6.)),
+                (Point::new(0., -7.), Point::new(2., -7.)),
+                (Point::new(7., -7.), Point::new(9., -7.)),
+                (Point::new(1., -8.), Point::new(3., -8.)),
+                (Point::new(6., -8.), Point::new(8., -8.)),
+                (Point::new(2., -9.), Point::new(7., -9.)),
+            ],
+        ),
+        ('b', vec![(Point::new(1., 1.), Point::new(1., 1.))]),
+    ];
+
+    let font = HashMap::from(font);
+    let letter: Vec<Point> = make_letter(font, 'c', coord, size);
+
+    letter
+}
+
+fn make_letter(
+    font: HashMap<char, Vec<(Point, Point)>>,
+    c: char,
+    coord: (f32, f32),
+    size: f32
+) -> Vec<(Point)> {
+    let mut points = Vec::new();
+    let lines = match font.get(&c) {
+        Some(review) => review,
+        None => panic!(),
+    };
+    for line in lines {
+        let x = line.0.x + coord.0;
+        let cline = create_line_float(
+            Point {
+                x: coord.0 + line.0.x * size,
+                y: coord.1 + line.0.y * (size/5.),
+            },
+            Point {
+                x: coord.0 + line.1.x * size,
+                y: coord.1 + line.1.y * (size/5.),
+            },
+            0.1,
+        );
+        for c in cline {
+            points.push(c)
+        }
+    }
+    points
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use crate::vector::{create_line_float, draw_points_float, Point, VertexF};
 
 use rand::{thread_rng, Rng};
 
+mod effects;
+
 const SAMPLE_RATE: u32 = 96000;
 const SAMPLE_RATE_F: f32 = SAMPLE_RATE as f32;
 const SIZE: i32 = 100;
@@ -26,33 +28,20 @@ fn main() -> Result<(), hound::Error> {
         sample_format: hound::SampleFormat::Int,
     };
     let amplitude = i16::MAX as f32;
-    let mut writer = hound::WavWriter::create("lines.wav", spec).unwrap();
+    let mut writer = hound::WavWriter::create("letters.wav", spec).unwrap();
     let mut scene: Vec<(f32, f32)> = Vec::new();
 
     ////////////////////////////////////////////
-    const LINES: usize = 16;
-    for i in (1..2000) {
-        let mut lines: Vec<Point> = Vec::new();
-        for j in 0..LINES {
-            let f = i as f32 / SIZE_F;
-            let point1 = Point {
-                x: -(0.01 * PI * 130. * f).sin() * (j * 10) as f32,
-                y: (0.01 * PI * 60. * f).sin() * 64.,
-            };
-            let point2 = Point {
-                x: (0.01 * PI * 60. * f).sin() * 64.,
-                y: (0.01 * PI * 60. * f).cos() * (j * 10) as f32,
-            };
-            let line = create_line_float(point1, point2, 0.99);
-            line.iter().for_each(|p| lines.push(*p));
-        }
-        let points = draw_points_float(1. / 50., lines, 5);
+    
+    for _i in 0..200 {
+        let c = effects::text::letter((1.,1.), 10.);
+        let points = draw_points_float(1. / 50., c, 5);
         for point in points {
             scene.push(point);
         }
     }
 
-    /////////////////////////////////////////////
+    ////////////////////////////////////////////
 
     for f in scene {
         writer.write_sample((f.0 * amplitude) as i16).unwrap();
