04fb42996fb6ca9d1e3ea23f6156b26bbbe9b24d
“eliaspoylio” <elias.poylio@gmail.com>
2023-08-01 17:08:20 +0300

    add title

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,14 @@ const DISTANCE_F: f32 = 50.;
 
 fn main() -> () {
     let spec = hound::WavSpec {
-        channels: 4,
+        channels: 2,
         sample_rate: SAMPLE_RATE,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
     let amplitude = i16::MAX as f32;
     let mut writer = hound::WavWriter::create("cesium.wav", spec).unwrap();
+    let mut title: Vec<(f32, f32)> = Vec::new();
     let mut scene: Vec<(f32, f32)> = Vec::new();
 
     ////////////////////////////////////////////
@@ -42,7 +43,8 @@ fn main() -> () {
 
     ////////////////////////////////////////////
 
-    let font_size = 1.1;
+    let font_size = 1.4;
+    let mut title_font_size = 1.5;
     let mut f_x_0 = 0.;
     let mut f_x_1 = 0.;
     let mut f_x_2 = 0.;
@@ -50,16 +52,55 @@ fn main() -> () {
     let f_y_1 = 0.;
     let f_y_2 = 0.;
     let f_y = 0.;
+
     let mut spread = 6.;
-    for i in 0..200 {
-        if i < 50 {
-            spread -= 0.1;
+    for i in 0..320 {
+        if i < 40 {
+            spread -= 0.125;
         }
-        if i == 50 {
+        if i == 40 {
             spread = 1.;
         }
-        if i > 150 {
-            spread += 0.1;
+        if i > 280 {
+            spread += 0.125;
+        }
+        title_font_size -= 0.001;
+        f_x_0 -= 0.0;
+        f_x_1 -= 0.0;
+        f_x_2 -= 0.0;
+        let mut cesium = effects::text::letter('c', (f_x_0 - 50., f_y_0), title_font_size, spread);
+        let mut e = effects::text::letter('e', (f_x_0 - 30., f_y_0), title_font_size, spread);
+        let mut s = effects::text::letter('s', (f_x_0 - 10., f_y_0), title_font_size, spread);
+        let mut i = effects::text::letter('i', (f_x_0 + 10., f_y_0), title_font_size, spread);
+        let mut u = effects::text::letter('u', (f_x_0 + 30., f_y_0), title_font_size, spread);
+        let mut m = effects::text::letter('m', (f_x_0 + 50., f_y_0), title_font_size, spread);
+        cesium.append(&mut e);
+        cesium.append(&mut s);
+        cesium.append(&mut i);
+        cesium.append(&mut u);
+        cesium.append(&mut m);
+
+        let mut c = -1;
+        cesium.retain(|_| {
+            c += 1;
+            return c % 2 == 0;
+        });
+        let points = draw_points_float(1. / 40., cesium, 8);
+        for point in points {
+            title.push(point);
+        }
+    }
+
+    let mut spread = 6.;
+    for i in 0..160 {
+        if i < 40 {
+            spread -= 0.125;
+        }
+        if i == 40 {
+            spread = 1.;
+        }
+        if i > 120 {
+            spread += 0.125;
         }
         f_x_0 -= 0.0;
         f_x_1 -= 0.0;
@@ -92,7 +133,7 @@ fn main() -> () {
             c += 1;
             return c % 2 == 0;
         });
-        let points = draw_points_float(1. / 50., text, 8);
+        let points = draw_points_float(1. / 40., text, 8);
         for point in points {
             if point.0 > -0.7 {
                 scene.push(point);
@@ -101,15 +142,15 @@ fn main() -> () {
     }
 
     spread = 6.;
-    for i in 0..200 {
-        if i < 50 {
-            spread -= 0.1;
+    for i in 0..160 {
+        if i < 40 {
+            spread -= 0.125;
         }
-        if i == 50 {
+        if i == 40 {
             spread = 1.;
         }
-        if i > 250 {
-            spread += 0.1;
+        if i > 120 {
+            spread += 0.125;
         }
         let mut by = effects::text::letter('b', (f_x_2 - 30., f_y_2), font_size, spread);
         let mut y = effects::text::letter('y', (f_x_2 - 15., f_y_2), font_size, spread);
@@ -130,7 +171,7 @@ fn main() -> () {
             return c % 2 == 0;
         });
         by.append(&mut spew);
-        let points = draw_points_float(1. / 50., by, 10);
+        let points = draw_points_float(1. / 40., by, 10);
         for point in points {
             if point.0 > -0.7 {
                 scene.push(point);
@@ -169,14 +210,22 @@ fn main() -> () {
         }
     }
 
+    let decay = 0.000001;
+    let mut factor = 1.;
+    for f in title {
+        writer.write_sample((f.0 * factor * amplitude) as i16).unwrap();
+        writer.write_sample((f.1 * factor * amplitude) as i16).unwrap();
+        factor -= decay;
+    }
+
     for pair in xm_channel.iter().zip_longest(scene.clone().iter()) {
         match pair {
             Both(l, r) => {
-            writer.write_sample((r.0 * amplitude) as i16).unwrap();
-            writer.write_sample((r.1 * amplitude) as i16).unwrap();
-            writer.write_sample((l * amplitude) as i16).unwrap();
-            writer.write_sample((l * amplitude) as i16).unwrap();
-            },
+                writer.write_sample((r.0 * amplitude) as i16).unwrap();
+                writer.write_sample((r.1 * amplitude) as i16).unwrap();
+                //writer.write_sample((l * amplitude) as i16).unwrap();
+                //writer.write_sample((l * amplitude) as i16).unwrap();
+            }
             Left(l) => (),
             Right(r) => (),
         }
